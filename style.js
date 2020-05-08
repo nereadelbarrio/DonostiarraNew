efbc0295bf654e565d6bbaf2f18e2bee526f0c59
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2020-05-08 13:39:22 +0200

    Update style.js

--- style.js
+++ style.js
@@ -92,12 +92,12 @@
 				{ name: 'Caja Mail', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-34' } },
 				{ name: 'CajaCabProj', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-35' } },
 
-				{ name: 'Tabla Color', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-2'} },
+				{ name: 'Tabla naranja', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-2'} },
 				{ name: 'Tabla 3', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-3'} },
 				{ name: 'Tabla verde', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-4'} },
 				{ name: 'Tabla morada', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-6'} },
 				
-				{ name: 'Celda Color', element: 'td', attributes: { 'class': 'bck-td-2'} },
+				{ name: 'Celda naranja', element: 'td', attributes: { 'class': 'bck-td-2'} },
 				{ name: 'Celda 3', element: 'td', attributes: { 'class': 'bck-td-3'} },
 				{ name: 'Celda verde', element: 'td', attributes: { 'class': 'bck-td-4'} },
 				{ name: 'Celda SinBorderTop', element: 'td', attributes: { 'class': 'bck-td-5'} },
