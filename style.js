b8df66fd308ddc98d71ef78c3c7f84d04a738648
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2020-05-12 17:09:14 +0200

    Update style.js

--- style.js
+++ style.js
@@ -65,7 +65,7 @@
 				{ name: 'Caja Prácticas', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-6' } },
 				{ name: 'Caja Desafío', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-7' } },
 				{ name: 'Caja Fíjate', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-8' } },
-				{ name: 'Caja Project', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-9' } },
+				{ name: 'Caja Desafío rosa', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-9' } },
 				{ name: 'Caja Pronunciation', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-10' } },
 				{ name: 'Caja ReadingsTools', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-11' } },
 				{ name: 'Caja CoreValues', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-12' } },
