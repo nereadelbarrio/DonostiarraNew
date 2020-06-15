ef1d2af9bb02404ede5c8e4085775fdb4893bbf2
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2020-06-15 14:54:35 +0200

    Update style.js

--- style.js
+++ style.js
@@ -50,7 +50,8 @@
 				{ name: 'Lista des 1 azul', element: 'ul', attributes: { 'class': 'bck-ul-1'} },
 				{ name: 'Lista des 2 negro', element: 'ul', attributes: { 'class': 'bck-ul-2'} },
 				{ name: 'Lista des 3 naranja', element: 'ul', attributes: { 'class': 'bck-ul-3'} },
-				{ name: 'Lista des 1 verde', element: 'ul', attributes: { 'class': 'bck-ul-4'} },
+				{ name: 'Lista des 4 verde', element: 'ul', attributes: { 'class': 'bck-ul-4'} },
+				{ name: 'Lista des 5 rosa', element: 'ul', attributes: { 'class': 'bck-ul-5'} },
 							
 				{ name: 'Lista 1 num', element: 'ol', attributes: { 'class': 'bck-ol-1' } },
 				{ name: 'Lista 2 num', element: 'ol', attributes: { 'class': 'bck-ol-2' } },
